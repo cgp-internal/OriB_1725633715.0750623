e9cd4e42975da01b1925ac901dce25ae8f9048a3
cgp-client <code.gen.pro.system@gmail.com>
2024-09-08 07:15:46 +0300

    Update middlewares/doorman-middleware.ts

--- middlewares/doorman-middleware.ts
+++ middlewares/doorman-middleware.ts
@@ -2,11 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import { appConfig } from "../utils/config";
 import { StatusCode } from "../models/status-enum";
 
-
 // only our FE app will have the key, which should be renew from time to time.
 function doorman(req: Request, res: Response, next: NextFunction) {    
     if (req.header("doormanKey") !== appConfig.doormanKey) {
-        res.status(StatusCode.Unauthorized).send("You are not authorized! (doorman)");
+        res.status(StatusCode.Unauthorized).send("You are not authorized!! (doorman)");
         return;
     }    
     next();  
